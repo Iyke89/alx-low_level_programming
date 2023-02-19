a3768bd173a6ec00fa1e2dd93d0ee67ac36d5520
Emeka Stanley <emekasi127@gmail.com>
2023-02-19 13:18:24 -0800

    werro

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
+ *
  * main - this is the main function
  *
- * Return: always 0
+ * Return:	0 on success
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"); 
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
