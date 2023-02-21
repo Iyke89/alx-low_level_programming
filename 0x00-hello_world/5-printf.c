4eaba81608d1ffcda61a8ba3705388571e2e39af
Emeka Stanley <emekasi127@gmail.com>
2023-02-20 17:42:03 -0800

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
+ * main - prints exactly hello world
  *
- * Return: 0 when  successfull
+ * Return: 0 when successfull
  */
 
 int main(void)
