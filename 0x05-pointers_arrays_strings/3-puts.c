50975c75dcb4bced0d8ad96be841139620f9b93d
Emeka Stanley <emekasi127@gmail.com>
2023-02-28 06:51:43 -0800

    print character

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - prints a string,followed by a new line, to stdout
@@ -10,8 +10,8 @@ void _puts(char *str)
 {
 	while (*str)
 	{
-		_putchar(*str);
+		putchar(*str);
 		str++;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
