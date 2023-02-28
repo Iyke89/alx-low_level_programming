3a7cefd4431457157474cf41ba376f987bc9c546
Emeka Stanley <emekasi127@gmail.com>
2023-02-28 08:53:40 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reverse
+ *
+ * Return: nothing on success
+ */
+void rev_string(char *s)
+{
+	int count = 0, i = 0;
+	char last_Char;
+
+	while (*s)
+	{
+		s++;
+		count++;
+	}
+	for (i = 0; i < count; i++)
+		s--;
+	for (i = 0; i < (count / 2); i++)
+	{
+		int last = count - 1;
+
+		last_Char = s[last - i];
+		s[last - i] = s[i];
+		s[i] = last_Char;
+	}
+}
