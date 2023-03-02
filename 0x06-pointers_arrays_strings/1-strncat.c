c08dcbf56ec91c280f157f12be392363ac5c329a
Emeka Stanley <emekasi127@gmail.com>
2023-03-02 05:13:08 -0800

    stringcat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,9 +14,12 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0 ; dest[i] != '\0' ; i++)
 		destlen++;
+
 	for (i = 0 ; src[i] != '\0' ; i++)
 		srclen++;
+
 	for (i = 0 ; i < n ; i++)
 		dest[destlen + i] = src[i];
+
 	return (dest);
 }
