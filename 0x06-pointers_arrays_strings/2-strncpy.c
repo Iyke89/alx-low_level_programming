809840a5856984a4b33f7aea8f964106ee516713
Emeka Stanley <emekasi127@gmail.com>
2023-03-02 07:47:34 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,23 @@
 #include "main.h"
 
 /**
- * *_strncpy - copies string
+ * _strncpy - concatenates two strings
  * @dest: destination string
  * @src: source string
- * @n: number of strings
- * Return: destination string
+ * @n: number of characters to be append
+ * Return: concatenate string
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
+	int destlen = 0;
+	int srclen = 0;
 	int i;
 
+	for (i = 0 ; dest[i] != '\0' ; i++)
+		destlen++;
+	for (i = 0 ; src[i] != '\0' ; i++)
+		srclen++;
 	for (i = 0 ; i < n ; i++)
-		dest[i] = src[i];
+		dest[destlen + 1] = src[i];
 	return (dest);
 }
